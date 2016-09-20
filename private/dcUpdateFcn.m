6f62100ceb567d94811a0c3151ff1de129e23c7a
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2016-09-20 11:19:46 +0200

    Added support for ZData and m(i) data cursor string

--- private/dcUpdateFcn.m
+++ private/dcUpdateFcn.m
@@ -1,8 +1,23 @@
 function str = dcUpdateFcn(~,e)
-x = e.Position(1);
-y = e.Position(2);
-data = e.Target.UserData;
-str = data.dcFun(data,x,y);
+pos = e.Position;
+x = pos(1);
+y = pos(2);
+if numel(pos) == 3;
+    z = pos(3);
+else
+    z = [];
+end
+UserData = e.Target.UserData;
+if iscell(UserData)
+    data = UserData{1};
+    m = UserData{2};
+else
+    data = UserData;
+end
+str = data.dcFun(data,x,y,z);
 if ~isempty(data.prefix), str = [data.prefix, str]; end
+if ~isempty(z) && exist('m','var')
+    str = [str, ['m(i): ' num2str(m(z))]];
+end
 if ~isempty(data.suffix), str = [str, data.suffix]; end
 end
