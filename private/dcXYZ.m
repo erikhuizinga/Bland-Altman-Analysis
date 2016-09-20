a1a9c4f9e1413c869c47442ae2147663f1e66858
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2016-09-20 11:20:48 +0200

    Update and rename dcXY.m to dcXYZ.m
    A ZData cursor string is now supported.

--- private/dcXYZ.m
+++ private/dcXYZ.m
@@ -1,5 +1,8 @@
-function str = dcXY(data,x,y)
+function str = dcXYZ(data,x,y,z)
 str = {};
 if ~isempty(data.xName), str = [str, [data.xName ': ' num2str(x)]]; end
 if ~isempty(data.yName), str = [str, [data.yName ': ' num2str(y)]]; end
+if ~isempty(z) && ~isempty(data.zName)
+    str = [str, [data.zName ': ' num2str(z)]];
+end
 end
