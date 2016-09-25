87f200659f869e014eb0736904e890fb14c4621f
Erik Huizinga <huizinga.erik@gmail.com>
2016-09-25 17:22:36 +0200

    Enhanced help text

--- ba.m
+++ ba.m
@@ -7,9 +7,9 @@ function varargout = ba(varargin)
 %   biostatistics and chemistry.
 %
 %   Syntax
-%   s = BA(x,y) performs Bland-Altman Analysis (BAA) on x and y, which are
-%   data from two measurement methods for the same quantity respectively. x
-%   and y can be of various classes and shapes:
+%   stats = BA(x,y) performs Bland-Altman Analysis (BAA) on x and y, which
+%   are data from two measurement methods for the same quantity
+%   respectively. x and y can be of various classes and shapes:
 %    - If x and y are vectors, regular BAA is performed. Every element in x
 %      corresponds to the element in y at the same index. These pairs are
 %      individual observations on individual subjects.
@@ -20,38 +20,40 @@ function varargout = ba(varargin)
 %      observations.
 %   For more information about BAA for repeated measurements, see section
 %   Bland-Altman Analysis for repeated measurements below. The calculations
-%   are done at a significance level of alpha = 0.05. Output s is a
+%   are done at a significance level of alpha = 0.05. Output stats is a
 %   structure containing multiple fields with descriptive statistics about
-%   the agreement of x and y. For more details on the fields in s see
+%   the agreement of x and y. For more details on the fields in stats see
 %   section Output below.
 %
-%   s = BA(x,y,alpha) specifies the significance level to calculate the
+%   stats = BA(x,y,alpha) specifies the significance level to calculate the
 %   limits of agreement and confidence intervals with. alpha must be a
 %   scalar in the interval [0,1]. If alpha is not specified a value of 0.05
 %   is used by default to calculate 95% limits of agreement and confidence
 %   intervals.
 %
-%   s = BA(__,Name,Value) specifies additional options using one or more
-%   Name-Value pair arguments, in addition to any of the input arguments in
-%   the previous syntaxes. For example, you can specify to create the
-%   mean-difference plot using the 'PlotMeanDifference' Name-Value pair
+%   stats = BA(__,Name,Value) specifies additional options using one or
+%   more Name-Value pair arguments, in addition to any of the input
+%   arguments in the previous syntaxes. For example, you can specify to
+%   create the mean-difference plot using the 'PlotMeanDifference'
+%   Name-Value pair argument.
+%
+%   BA(__) can be used to create graphs without returning an output
 %   argument.
 %
-%   BA(__) can be used to plot the data without returning an output
-%   argument.
-%
-%   __ = BA(f,__) specifies the figure(s) f in which to create the plots
+%   __ = BA(f,__) specifies the figure(s) f in which to create the graphs
 %   specified with the corresponding Name-Value pairs. The number of
-%   figures in f must equal one or the number of specified plots.
+%   figures in f must equal one or the number of specified plots. If f is
+%   the handle to only one figure, a number of subplots is created in the
+%   figure equal to the number of graphs requested.
 %
 %   __ = BA(ax,__) specifies the (array of) axes in which to create the
-%   plots specified with the corresponding Name-Value pairs. The number of
-%   axes in ax must equal the number of specified plots.
+%   requested graphs with the corresponding Name-Value pairs. The number of
+%   axes in ax must equal the number of graphs requested.
 %
 %   Examples
-%   See and run the ba1999demo.m script for examples of the syntax of BA
-%   used with data from the 1999 article by Bland and Altman. Calling BA
-%   without input arguments also runs the demonstration script.
+%   See and run the bademo.m script for examples of the syntax of BA used
+%   with data from the 1999 article by Bland and Altman. Calling BA without
+%   input arguments also runs the demonstration script.
 %
 %   Name-Value Pair Arguments
 %   Specify optional comma-separated pairs of Name,Value arguments to
@@ -90,7 +92,9 @@ function varargout = ba(varargin)
 %   Altman suggest in their 1999 article (see p. 144) only the logarithmic
 %   transformation should be used, i.e. specify 'Transform',@log. Other
 %   transforms are not easily relatable to the actual measurements, hence
-%   their recommendation.
+%   their recommendation. Note that no backtransformation is performed on
+%   the statistics in the optional output argument, i.e. they are the
+%   transformed statistics.
 %   Example: 'Transform',@log transforms x to log(x) and y to log(y).
 %
 %   'PlotMeanDifference': Create mean-difference graph
@@ -105,22 +109,29 @@ function varargout = ba(varargin)
 %   false (default) | true
 %   Creat the mean-ratio graph if the specified value is true. The
 %   mean-ratio graph is a scatter plot of the ratio between observations
-%   versus their mean.
+%   versus their mean. If the mean-ratio graph is created and an output
+%   arguments is specified, the output argument contains a field called
+%   ratio with the ratio statistics.
 %
 %   'PlotCorrelation': Create correlation graph
 %   false (default) | true
 %   Create the correlation graph if the specified value is true. The
 %   correlation graph is a scatter plot of x and y. Specifying the
 %   'PlotAll' Name-Value pair argument as true creates the correlation
-%   plot, regardless of the 'PlotCorrelation' value.
+%   plot, regardless of the 'PlotCorrelation' value. If the correlation
+%   graph is created and an output arguments is specified, the output
+%   argument contains a field called correlation with the correlation
+%   statistics.
 %
-%   'PlotDefault': Create default plots
+%   'PlotDefault': Create default graphs
 %   false (default) | true
-%   Create mean-difference and correlation plots if the specified value is
+%   Create mean-difference and correlation graphs if the specified value is
 %   true. Setting 'PlotDefault' to true overrides any value given to the
 %   'PlotMeanDifference' and 'PlotCorrelation' Name-Value pair arguments.
 %   However, setting 'PlotDefault' to false does not override the
-%   individual plot Name-Value pair arguments.
+%   individual Name-Value pair arguments. Setting 'PlotDefault' to true and
+%   specifying the optional output argument adds the correlation field to
+%   the output argument.
 %
 %   'PlotStatistics': Add statistics to the created plots
 %   'none' (default) | 'basic' | 'extended'
@@ -147,18 +158,18 @@ function varargout = ba(varargin)
 %   false (default) | true
 %   Assume constant residual variance in the simple linear regression
 %   performed if the 'PlotStatistics','regression' Name-Value pair argument
-%   is specified. This mean the upper and lower limits of agreement lines
+%   is specified. This means the upper and lower limits of agreement lines
 %   will have the same slope as the bias line. This assumption holds if the
 %   slope of the upper and lower limits of agreement do not differ
 %   significantly from the slope of the bias regression line.
 %   
-%   'ConstantTrueValue': Assume constant true value
+%   'ConstantTrueValue': Assume the true value is constant
 %   true (default) | false
 %   Assume the true value being measured by x and y is constant. This
 %   assumption is true for repeated measurements of the same constant
 %   quantity. For example, repeated measurements of a subject's blood
-%   pressure within the same minute can be assumed to be constant. If this
-%   is to be assumed in the calculations, specify the
+%   pressure within the same minute could be assumed to be constant. If
+%   this is to be assumed in the calculations, specify the
 %   'ConstantTrueValue',true pair argument. If the repeated measurements
 %   are of a quantity that changes from measurement to measurement, this
 %   assumption does not hold. For example, blood pressure measured on
@@ -167,40 +178,40 @@ function varargout = ba(varargin)
 %   assumed, specify the 'ConstantTrueValue',false pair argument.
 %
 %   Output
-%   The only output argument s is optional. It is a scalar structure
+%   The only output argument stats is optional. It is a scalar structure
 %   containing multiple fields with descriptive statistics about the
-%   agreement of x and y. The number of fields in s varies depending on the
-%   input arguments. By default s contains three fields, being difference,
-%   x and y. Additional fields in s are ratio and correlation. They exist
-%   depending on the requested graphs. If the mean-ratio graph is requested
-%   using the 'PlotMeanRatio' Name-Value pair argument, then s is returned
-%   with the ratio field. If the correlation plot is requested using the
-%   'PlotCorrelation' or 'PlotDefault' Name-Value pair arguments, then s is
-%   returned with the correlation field. Note that the difference field is
-%   returned regardless of the 'PlotMeanDifference' Name-Value pair
-%   argument.
+%   agreement of x and y. The number of fields in stats varies depending on
+%   the input arguments. By default stats contains three fields, being
+%   difference, x and y. Additional fields in stats are ratio and
+%   correlation. They exist depending on the requested graphs. If the
+%   mean-ratio graph is requested using the 'PlotMeanRatio' Name-Value pair
+%   argument, then stats is returned with the ratio field. If the
+%   correlation plot is requested using the 'PlotCorrelation' or
+%   'PlotDefault' Name-Value pair arguments, then stats is returned with
+%   the correlation field. Note that the difference field is returned
+%   regardless of the 'PlotMeanDifference' Name-Value pair argument.
 %
 %   Fields difference and ratio are described together below, after which
 %   fields x and y are described together too. Lastly, field correlation is
 %   described.
 %
 %   The difference and ratio fields are structures themselves, containing
-%   the statistics about the differences and ratios (resp.) between
+%   the statistics about the differences and ratios repectively between
 %   observations in x and y. The fields in difference and ratio are
 %   described below (the word statistic refers to either difference or
 %   ratio):
 %
 %   mu: the mean statistic between x and y, also called the bias.
-%   Example: s.difference.mu is the mean difference.
+%   Example: stats.difference.mu is the mean difference.
 %
 %   muCI: the 95% (default, depending on alpha) confidence interval of
 %   the mean statistic.
-%   Example: s.ratio.muCI is the confidence interval of the mean ratio.
+%   Example: stats.ratio.muCI is the confidence interval of the mean ratio.
 %
 %   loa: the 95% (default, depending on alpha) limits of agreement, a 2
 %   element vector. The first element is the lower limit of agreement, the
 %   second is the upper.
-%   Example: s.difference.loa(1) is the lower limit of agreement of the
+%   Example: stats.difference.loa(1) is the lower limit of agreement of the
 %   differences.
 %
 %   loaCI: the 95% (default, depending on alpha) confidence interval of the
@@ -208,52 +219,57 @@ function varargout = ba(varargin)
 %   lower limit of agreement, the second to the upper limit. The first and
 %   second row correspond to the lower and upper confidence interval bound
 %   respectively.
-%   Ecample: s.ratio.loaCI(:,2) is the confidence interval of the upper
+%   Ecample: stats.ratio.loaCI(:,2) is the confidence interval of the upper
 %   limit of agreement of the ratios.
 %
 %   s: the standard deviation of the statistic.
-%   Example: s.difference.s is the standard deviation of the differences.
+%   Example: stats.difference.s is the standard deviation of the
+%   differences.
 %
 %   rSMu: the Spearman rank correlation between mean and statistic.
-%   Example: s.ratio.rSMu is the Spearman rank correlation between mean and
-%   the ratios.
+%   Example: stats.ratio.rSMu is the Spearman rank correlation between mean
+%   and the ratios.
 %
 %   pRSMu: the p-value of the Spearman rank correlation for testing the
 %   hypothesis of no correlation against the alternative that there is a
-%   nonzero correlation. %TODO also output rhoXY,pRhoXY
-%   Example: s.difference.pRSMu is the p-value of the Spearman rank
+%   nonzero correlation. %TODO also output rhoXY,pRhoXY in the correlation field
+%   Example: stats.difference.pRSMu is the p-value of the Spearman rank
 %   correlation between mean and difference, to test the hypothesis of no
 %   correlation against the alternative of nonzero correlation.
 %
 %   polyMu: the polynomial coefficients of the simple linear regression of
 %   the statistic on the mean. The first element of polyMu is the slope,
 %   the second the intercept.
-%   Example: s.ratio.polyMu(2) is the intercept of the simple linear
+%   Example: stats.ratio.polyMu(2) is the intercept of the simple linear
 %   regression line of the ratio on the mean.
 %
 %   msePolyMu: the mean squared error (MSE) of the simple linear regression
 %   of the statistic on the mean.
-%   Example: s.difference.msePolyMu is the MSE of the simple linear
+%   Example: stats.difference.msePolyMu is the MSE of the simple linear
 %   regression of the difference on the mean.
 %
-%   The x y fields of structure s contain statistics about inputs x and y,
-%   that are calculated in BAA. The fields in x and y are structure
+%   The x and y fields of structure stats contain statistics about inputs x
+%   and y that are calculated in BAA. The fields in x and y are structures
 %   themselves and have the following fields (the word input referring to x
 %   or y):
 %
 %   varWithin: the within-subject variance of the input. varWithin equals
-%   zero for regular BAA, but can be non-zero in BAA for repeated
-%   measurements.
-%   Example: s.x.varWithin is the within-subject variance of input x.
+%   zero for BAA without repeated measurements, but can be non-zero in BAA
+%   for repeated measurements.
+%   Example: stats.x.varWithin is the within-subject variance of input x.
 %
-%   If the correlation field exists in s, it contains the following fields:
+%   If the correlation field exists in stats, it contains the following
+%   fields:
 %
-%   rho: the Pearson correlation coefficient of the inputs.
+%   rho: the Pearson correlation coefficient between inputs x and y.
 %
-%   p: the p-value of the Pearson correlation coefficient of the inputs.
+%   p: the p-value of the Pearson correlation coefficient between inputs x
+%   and y.
 %
 %   poly: the polynomial coefficients of the simple linear regression of y
 %   on x. The first element in poly is the slope, the second the intercept.
+%   Example: stats.correlation.poly(1) is the slope of the simple linear
+%   regression line of y on x.
 %
 %   polyMSE: the mean squared error (MSE) of the simple linear regression
 %   of y on x.
@@ -264,44 +280,47 @@ function varargout = ba(varargin)
 %   One of these assumptions is that the individual observation pairs are
 %   independent. This is the case when every observed pair comes from a
 %   different subject. However, when multiple measurements are taken on the
-%   same individual, this assumption does not hold. The measurements on the
-%   same individual might depend on each other. For example, a subject's
-%   blood pressure is measured every minute using a sphygmomanometer. The
-%   observations of every minute will be quite correlated with the previous
-%   minute, i.e. the cross-covariance is high at the first (few) lag(s).
-%   Another more general example: consider 10 observations of a physical
-%   quantity. When these 10 observations are obtained from 10 different
-%   subjects (assuming these subjects are a representative sample of the
-%   population), the assumptions of regular BAA will have been met.
-%   However, if these 10 observations would come from one subject only, the
-%   calculations of (mean) difference (or ratio) will depend strongly on
-%   the within-subject variance of this particular subject. This is less
-%   informative about the performance of the two measurement methods and
-%   how they compare in general. Instead, this analysis tells us about the
-%   performance of the methods on this subject, which usually is not of
-%   interest in method comparison studies.
+%   same individual, this assumption migtht not hold. The measurements on
+%   the same individual might depend on each other. For example, a
+%   subject's blood pressure is measured every minute using a
+%   sphygmomanometer. The observations of every minute will be quite
+%   correlated with the previous minute, i.e. the cross-covariance is high
+%   at the first (few) lag(s). Another more general example: consider 10
+%   observations of a physical quantity. If these 10 observations are
+%   obtained from 10 different subjects (assuming these subjects are a
+%   representative sample of the population), the assumption of
+%   uncorrelatedness in regular BAA will have been met. However, if these
+%   10 observations would come from one subject only (as an extreme
+%   example), the calculations of (mean) difference (or ratio) will depend
+%   strongly on the within-subject variance of this particular subject.
+%   This is less informative about the performance of the two measurement
+%   methods in the population of interest and how they compare in general.
+%   Instead, this analysis tells us about the performance of the methods on
+%   this particular subject, which usually is not of interest in method
+%   comparison studies.
 %
-%   About
-%   This MATLAB function is an implementation of the methods in the 1999
-%   article by Bland and Altman:
-%   Bland & Altman 1999 - Measuring agreement methods in comparison studies
-%   http://smm.sagepub.com/content/8/2/135.abstract
+%   About & References
+%   This MATLAB function is an implementation of the methods in the 1983,
+%   1986, 1999 and 2007 articles by Bland and Altman, especially these two:
+%   
+%   [BA1999] Bland, J.M., and Altman, D.G. 1999. Measuring agreement in
+%   method comparison studies. Statistical Methods in Medical Research 8:
+%   135–160. doi:10.1191/096228099673819272.
+%   [BA2007] Bland, J.M., and Altman, D.G. 2007. Agreement Between Methods
+%   of Measurement with Multiple Observations Per Individual. Journal of
+%   Biopharmaceutical Statistics 17: 571–582.
+%   doi:10.1080/10543400701329422.
 %
-%   You might not have access to this article. Access it through your
-%   institution's library or buy it.
+%   You might not have access to these articles. Access them through your
+%   institution's library or buy them to read them.
 %
-%   The article comprises of 5 methodological sections (sections 2-6). The
-%   current version of this MATLAB file (8 september 2016) implements the
-%   first methodological section. More sections will be added in the
-%   future.
+%   The demonstration script bademo.m is an implementation of the
+%   calculations done by Bland and Altman in their articles. Their articles
+%   contain a number of example data sets, which they use in their methods
+%   to demonstrate them. The demonstration script illustrates the same
+%   results and the syntax used to obtain them.
 %
-%   The demonstration script `ba1999demo.m` is an implementation of the
-%   calculations done by Bland and Altman in the article. Their article
-%   contains a number of example data sets, which they use in their
-%   methods. The demonstration script illustrates the same results and the
-%   syntax used to obtain them.
-%
-%   See also BA1999DEMO
+%   See also BADEMO
 
 %   Copyright (C) 2016 Erik Huizinga, huizinga.erik@gmail.com
 %
