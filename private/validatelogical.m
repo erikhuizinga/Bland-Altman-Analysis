2386be6d4275759ad6e1b2bb35ec08784b84312e
Erik Huizinga <huizinga.erik@gmail.com>
2016-10-23 14:53:44 +0200

    Apply PEP8 Style Guide to private/validatelogical.m
    Find the guide here:
    https://www.python.org/dev/peps/pep-0008/

--- private/validatelogical.m
+++ private/validatelogical.m
@@ -1,5 +1,5 @@
 function tf = validatelogical(L)
-%VALIDATELOGICAL True for arrays that can be converted to logical.
+%VALIDATELOGICAL Return true for arrays that can be converted to logical
 %   VALIDATELOGICAL(L) returns logical 1 (true) if L is a logical or
 %   numeric array and logical 0 (false) otherwise.
 %
