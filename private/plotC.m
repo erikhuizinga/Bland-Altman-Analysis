58d11cf9f4e04ca9256c7af280a146e17847ea14
Erik Huizinga <huizinga.erik@gmail.com>
2016-10-23 21:37:47 +0200

    Apply PEP8 Style Guide to private/plotC.m
    Find the guide here:
    https://www.python.org/dev/peps/pep-0008/

--- private/plotC.m
+++ private/plotC.m
@@ -1,52 +1,55 @@
-function plotC(axC,x,y,doPlotBasicStats,pRhoXY,rhoXY,n,xName,yName)
-% correlation plot of observations not accounting for repeated measurements
+function plotC(axC, x, y, doPlotBasicStats, pRhoXY, rhoXY, n, xName, yName)
+% Create correlation plot of observations not accounting for repeated
+% measurements
 
-% preparation
+% Prepare
 axes(axC)
 legEntries = gobjects(0);
 
-% plot y against x
-sC = scatter(x,y);
-sC.ZData = 1:n;
-sC.UserData = dcStruct([],'M1','M2','j',[],@dcXYZ);
+% Plot y against x
+sC = scatter(x, y);
+sC.ZData = 1 : n;
+sC.UserData = dcStruct([], 'M1', 'M2', 'j', [], @dcXYZ);
 
 if doPlotBasicStats
-    % add correlation to legend
-    if pRhoXY<1e-4
-        % as recommended by BMJ 1996;312:572
-        % http://dx.doi.org/10.1136/bmj.312.7030.572
+    % Add correlation to legend
+    if pRhoXY < 1e-4
+        % Change string when pRhoXY < 1e-4, as recommended by BMJ
+        % 1996;312:572: http://dx.doi.org/10.1136/bmj.312.7030.572
         strPRhoXY = sprintf('\\itp\\rm < 0.0001');
+        
     else
-        strPRhoXY = sprintf('\\itp\\rm = %.2f',pRhoXY);
+        strPRhoXY = sprintf('\\itp\\rm = %.2f', pRhoXY);
     end
-    sC.DisplayName = sprintf('\\rho = %.2f (%s)',rhoXY,strPRhoXY);
-    legEntries(end+1) = sC;
+    
+    sC.DisplayName = sprintf('\\rho = %.2f (%s)', rhoXY, strPRhoXY);
+    legEntries(end + 1) = sC;
 end
 
-% y = x reference line
-eqLine = refline(1,0); % 45Â° degree, because of axis equal
-eqLine.Color = [.75 .75 .75];
+% Add y = x reference line
+eqLine = refline(1, 0);  % Line at 45°, because axis are equal
+eqLine.Color = [.75, .75, .75];
 eqLine.LineStyle = '--';
 eqLine.DisplayName = 'line of equality';
-eqLine.UserData = dcStruct([],[],[],[],'M2 = M1',@dcXYZ);
-legEntries(end+1) = eqLine;
+eqLine.UserData = dcStruct([], [], [], [], 'M2 = M1', @dcXYZ);
+legEntries(end + 1) = eqLine;
 
-% axes labels
+% Add axes labels
 xlabel('M_1')
 ylabel('M_2')
 title(sprintf(['Scatter plot of (%u observation pairs):\n' ...
-    ' \\rm\\itM_1\\rm: %s, \\itM_2\\rm: %s'],n,xName,yName))
+               ' \\rm\\itM_1\\rm: %s, \\itM_2\\rm: %s'], n, xName, yName))
 
-% legend
-legend(legEntries,'Location','SouthEast')
+% Add legend
+legend(legEntries, 'Location', 'SouthEast')
 
-% reorder plot children
-axC.Children = axC.Children([end 1:end-1]);
+% Reorder plot children
+axC.Children = axC.Children([end, 1 : end-1]);
 
-% equalise axes
+% Equalise axes
 axis tight
 axis equal
 
-% set outer position
+% Set outer position
 setOP(axC)
 end
